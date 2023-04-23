6c8a3ccfd5103d8ed1094fc27ba7566d5c31855f
Jess2315 <munozjb@globalhitss.com>
2023-04-22 20:31:05 -0500

    Componente inicio
    se agrega componente pagina de inicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { LoadingComponent } from './components/loading/loading.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoadingComponent
+    LoadingComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule
